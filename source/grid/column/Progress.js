8af632c183426326f374e2368f7aea8c060a1b51
Phil Crawford <zombeerose@yahoo.com>
2012-08-22 10:57:23 -0700

    * Add switch for supporting decimal values or whole numbers. * Add support for incremental brackets that apply different CSS classes. * Add corresponding CSS classes with bg gradients.

--- source/grid/column/Progress.js
+++ source/grid/column/Progress.js
@@ -20,7 +20,7 @@
  * 
  * @author Phil Crawford
  * @license Licensed under the terms of the Open Source [LGPL 3.0 license](http://www.gnu.org/licenses/lgpl.html).  Commercial use is permitted to the extent that the code/component(s) do NOT become part of another Open Source or Commercially licensed development library or toolkit without explicit permission.
- * @version 0.1 (June 30, 2011)
+ * @version 1.0 (August 22, 2012)
  * @param {Object} config 
  */
 Ext.define('Ext.ux.grid.column.Progress', {
@@ -28,6 +28,26 @@ Ext.define('Ext.ux.grid.column.Progress', {
     alias: 'widget.dvp_progresscolumn',
     
     cls: 'x-progress-column',
+    
+    /**
+     * @cfg {Array} brackets
+     * Defines the different low & high ranges that correspond to each progress step. 
+     * Specify either 2 values corresponding with low & high, or specify a single number 
+     * that corresponds with the low value.  The high value will be assumed to be Number.MAX_VALUE.
+     */
+    brackets: [
+        [0,24],
+        [25,49],
+        [50,74],
+        [75,99],
+        [100]
+    ],
+    /**
+     * @cfg {Boolean} decimalValues
+     * Set to true if the value being rendered will be a decimal between 0 and 1, such as .5, or  
+     * set to false if the value will be an integer between 0 and 100, like 50. 
+     */
+    decimalValues: true,
     
     groupable: false,
     
@@ -37,6 +57,14 @@ Ext.define('Ext.ux.grid.column.Progress', {
      * Defaults to 'x-progress' 
      */
     progressCls: 'x-progress',
+    /**
+     * @cfg {String} progressStepCls
+     * The class used for custom styling of each step.  The class should include a placeholder for the bracket number 
+     * that the rendered value corresponds with.
+     * 
+     * The step indexes are zero-based, which should correspond with the CSS class names starting with 'x-progress-step-0'
+     */
+    progressStepCls: 'x-progress-step-{0}',
     /**
      * @cfg {String} progressText
      * The text for displaying the current percentage complete.  Includes a placeholder {0} for the number.
@@ -54,25 +82,63 @@ Ext.define('Ext.ux.grid.column.Progress', {
     defaultRenderer: function(v, meta, record) {
         var me = this,
             cls = me.progressCls,
-            text, newWidth;
+            stepCls, text, newWidth, decimal, whole;
         
-        newWidth = Math.floor(v * me.getWidth(true)); //me = column
+        decimal = me.decimalValues ? v : (v / 100);
+        whole = me.decimalValues ? (v * 100) : v;
+        
+        newWidth = Math.floor(decimal * me.getWidth(true)); //me = column
         
         // Allow a configured renderer to create initial value (And set the other values in the "metadata" argument!)
-        v = Ext.isFunction(me.origRenderer) ? me.origRenderer.apply(scope, arguments) || v : v;
+        v = Ext.isFunction(me.origRenderer) ? me.origRenderer.apply(me, arguments) || v : v;
         
-        text = Ext.String.format(me.progressText,Math.round(v*100));
+        text = Ext.String.format(me.progressText,Math.round(whole));
+        
+        stepCls = me.getStepCls(whole);
         
         meta.tdCls += ' ' + cls + ' ' + cls + '-' + me.ui;
         v = '<div class="' + cls + '-text ' + cls + '-text-back">' +
                 '<div>' + text + '</div>' +
             '</div>' +
-            '<div class="' + cls + '-bar" style="width: '+ newWidth + 'px;">' +
+            '<div class="' + cls + '-bar ' + stepCls + '" style="width: '+ newWidth + 'px;">' +
                 '<div class="' + cls + '-text">' +
                     '<div>' + text + '</div>' +
                 '</div>' +
-            '</div>' 
+            '</div>';
+
         return v;
+    },
+    
+    /**
+     * @private
+     * @param {Number} value
+     * @return {String}
+     */
+    getStepCls: function(v){
+        var me = this,
+            brackets = me.brackets,
+            bracket,
+            low,
+            high,
+            i = 0;
+            
+        for (; i < brackets.length; i++){
+            bracket = brackets[i];
+            //<DEBUG>
+            if (!Ext.isArray(bracket) || Ext.isEmpty(bracket)){
+                Ext.Error.raise('Invalid bracket configuration.  Each bracket must be an array containing 1 or 2 numeric values.');
+            }
+            if (bracket.length > 2){
+                Ext.Error.raise('Invalid bracket configuration.  Each bracket should contain 2 values at most.');
+            }
+            //</DEBUG>
+            low = bracket[0];
+            high = (bracket.length > 1) ? bracket[1] : Number.MAX_VALUE;
+            if (v >= low && v <= high){
+                break;
+            }
+        }
+        return Ext.String.format(me.progressStepCls,i);
     }
 }); //eo extend
 
